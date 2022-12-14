fcb4b182a173c1fdeb58d79607ff4a92e784d95b
QuickMash <106212829+QuickMash@users.noreply.github.com>
2022-12-14 08:34:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var x = '';
-prompt("Username", x+"");
+if (document.addEventListener) {
+  document.addEventListener('contextmenu', function(e) {
+    alert("You've tried to open context menu"); //here you draw your own menu
+    e.preventDefault();
+  }, false);
+} else {
+  document.attachEvent('oncontextmenu', function() {
+    alert("You've tried to open context menu");
+    window.event.returnValue = false;
+  });
 }
-if x = QuickMash then {
-alert("Thats right");
-}else{
-  alert("Um, thats incorrect")
